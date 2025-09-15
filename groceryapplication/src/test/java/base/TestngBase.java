e7849e29690b5f3f671cf8a61d61f0a195095f5b
user <user@LAPTOP-66KHMHA0>
2025-09-15 15:10:37 +0530

    Commit

---
+++ groceryapplication/src/test/java/base/TestngBase.java
@@ -0,0 +1,37 @@
+package base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestngBase {
+	public WebDriver driver;
+	@BeforeMethod
+	public void intializebrowser() {
+		//driver= new ChromeDriver();
+		//driver=new FirefoxDriver();
+		//driver=new EdgeDriver();
+		ChromeOptions options = new ChromeOptions();
+		Map<String,Object> prefs=new HashMap<>();
+		prefs.put("profile.password_manager_leak_detection", false);
+		options.setExperimentalOption("prefs", prefs);
+		driver=new ChromeDriver(options);
+		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		driver.manage().window().maximize();
+	}
+	@AfterMethod
+	public void browserclose() {
+		//driver.close();//tab
+		//driver.quit();// window
+	}
+
+	
+
+}
+
+
