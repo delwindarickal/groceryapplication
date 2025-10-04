ae5eb701b5a05722f736369551994362a74acf58
user <user@192.168.1.10>
2025-10-05 01:09:25 +0530

    Parallel, cross, multi, url

--- groceryapplication/src/test/java/base/TestngBase.java
+++ groceryapplication/src/test/java/base/TestngBase.java
@@ -1,35 +1,68 @@
 package base;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
+import constant.Constants;
 import utilities.ScreenshotUtility;
 
 public class TestngBase {
+	Properties prop;
+	FileInputStream f;
 	public WebDriver driver;
-	@BeforeMethod
-	public void intializebrowser() {
+	@BeforeMethod(alwaysRun = true)
+	@Parameters("browser")
+	public void intializebrowser(String browser) throws Exception {
 		//driver= new ChromeDriver();
 		//driver=new FirefoxDriver();
 		//driver=new EdgeDriver();
-		ChromeOptions options = new ChromeOptions();
-		Map<String,Object> prefs=new HashMap<>();
-		prefs.put("profile.password_manager_leak_detection", false);
-		options.setExperimentalOption("prefs", prefs);
-		driver=new ChromeDriver(options);
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		//ChromeOptions options = new ChromeOptions();
+		//Map<String,Object> prefs=new HashMap<>();
+		//prefs.put("profile.password_manager_leak_detection", false);
+		//options.setExperimentalOption("prefs", prefs);
+		//driver=new ChromeDriver(options);
+		
+		
+		prop =new Properties();
+		f = new FileInputStream(Constants.CONGIGFILE);
+		prop.load(f);
+		if(browser.equalsIgnoreCase("Chrome")) {
+			//driver=new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			Map<String,Object> prefs=new HashMap<>();
+			prefs.put("profile.password_manager_leak_detection", false);
+			options.setExperimentalOption("prefs", prefs);
+			driver=new ChromeDriver(options);
+		}
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			driver=new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			driver=new EdgeDriver();
+		}
+		else {
+			throw new Exception("Invalid browser name");
+		}
+		driver.get(prop.getProperty("url"));
+		//driver.get("https://groceryapp.uniqassosiates.com/admin/login");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
+	
 	@AfterMethod
 	public void browserclose() {
 		//driver.close();//tab
